4f47a9824d154e9e8576f7b6c37151abaf84b932
Oilwoo <ooo952@naver.com>
2025-02-06 00:09:00 +0900

    init

---
+++ src/main/java/com/gdg/poppet/PoppetApplication.java
@@ -0,0 +1,13 @@
+package com.gdg.poppet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoppetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoppetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gdg/poppet/PoppetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdg.poppet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoppetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
