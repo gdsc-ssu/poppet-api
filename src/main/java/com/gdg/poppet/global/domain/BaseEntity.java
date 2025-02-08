3fd55edb24998d5f2d149ed68f52dd01cf146154
daeun084 <030804jk@naver.com>
2025-02-08 19:38:55 +0900

    chore: base entity annotation 추가

--- src/main/java/com/gdg/poppet/global/domain/BaseEntity.java
+++ src/main/java/com/gdg/poppet/global/domain/BaseEntity.java
@@ -1,12 +1,17 @@
 package com.gdg.poppet.global.domain;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
-
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@MappedSuperclass
 public class BaseEntity {
     @CreatedDate
     @Column(updatable = false)
@@ -14,9 +19,4 @@ public class BaseEntity {
 
     @LastModifiedDate
     private LocalDateTime updatedAt;
-
-    public String getCreatedAtFormatted() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd");
-        return createdAt.format(formatter);
-    }
 }
