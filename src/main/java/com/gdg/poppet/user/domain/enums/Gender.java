9a413b8ee712e90252a5cce0ad173030b3fc4559
Oilwoo <ooo952@naver.com>
2025-02-21 00:58:39 +0900

    feat: Gender 문자열로 enum 반환 기능 추가

--- src/main/java/com/gdg/poppet/user/domain/enums/Gender.java
+++ src/main/java/com/gdg/poppet/user/domain/enums/Gender.java
@@ -1,5 +1,20 @@
 package com.gdg.poppet.user.domain.enums;
 
 public enum Gender {
-    MALE, FEMALE
+    MALE, FEMALE;
+
+    public static Gender fromString(String value) {
+        if (value == null) {
+            throw new IllegalArgumentException("gender cannot be null");
+        }
+
+        switch (value.trim().toLowerCase()) {
+            case "male":
+                return MALE;
+            case "female":
+                return FEMALE;
+            default:
+                throw new IllegalArgumentException("Invalid gender value: " + value);
+        }
+    }
 }
