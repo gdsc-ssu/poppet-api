703ef3cb057724dd5825ca14804867a5bf258cc6
daeun084 <030804jk@naver.com>
2025-04-18 23:45:54 +0900

    feat: 이미지 인라인으로 넣어 이메일 전송

--- src/main/java/com/gdg/poppet/email/infra/application/EmailSendEventListener.java
+++ src/main/java/com/gdg/poppet/email/infra/application/EmailSendEventListener.java
@@ -1,10 +1,12 @@
 package com.gdg.poppet.email.infra.application;
 
+import com.gdg.poppet.chat.domain.model.ChatRoom;
 import com.gdg.poppet.email.application.event.EmailSendEvent;
 import com.gdg.poppet.email.domain.model.Email;
 import com.gdg.poppet.user.domain.model.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
@@ -16,16 +18,18 @@ import org.springframework.transaction.event.TransactionalEventListener;
 public class EmailSendEventListener {
 
     private final EmailSendService emailSendService;
+    private final EmailTemplateService emailTemplateService;
 
     @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handleEmailSendEvent(EmailSendEvent event) {
         User user = event.getUser();
-        String summary = event.getChatRoom().getSummary();
+        ChatRoom chatRoom = event.getChatRoom();
+        ByteArrayResource body = emailTemplateService.makeEmailBackground(user, chatRoom);
 
         for (Email email : user.getEmails()) {
             try {
-                emailSendService.sendEmail(email.getEmailAddress(), summary);
+                emailSendService.sendEmail(email.getEmailAddress(), body);
             } catch (Exception e) {
                 log.error("[*] {}으로 이메일 전송 중 오류 발생", email.getEmailAddress(), e);
             }
